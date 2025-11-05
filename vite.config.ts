ebfb495b9a672e82864eac81290426200037525f
Rodrigo Farias <rodrigo.leal.melo.farias@gmail.com>
2025-11-04 23:52:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/chaheloisaerodrigo/", // 👈 ADICIONE ESTA LINHA
   server: {
     host: "::",
     port: 8080,
